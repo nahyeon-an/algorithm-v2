cd87a7e0dcfc6989ac35cfc32a09def4e8ea1d25
nahyeon-an <skgus923@gmail.com>
2025-02-02 21:12:35 +0900

    (02-backtracking) Solve leetcode - combination sum

---
+++ 02.backtracking/solve_py/leetcode_40.py
@@ -0,0 +1,43 @@
+"""
+candidates : not-distinct integers 로 구성된 배열
+합이 target 이 되는 candidates 의 유니크한 조합 리스트를 리턴해라
+이 때 한 원소가 한번만 선택될 수 있음
+
+1 <= candidates.length <= 100
+"""
+from symbol import comp_if
+from typing import List
+
+
+def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
+    results = []
+
+    # sort
+    candidates.sort()
+
+    def recursive(c: List[int], t: int, chosen: List[int]):
+        if t == 0:
+            return chosen
+
+        for idx, n in enumerate(c):
+            if n > t:
+                continue
+
+            # n(이전에 선택한 숫자) 보다 작은 숫자는 candidates 에서 빼야함
+            if chosen and chosen[-1] > n:
+                continue
+
+            # 이전에 선택된 숫자와 같으면 제외
+            if idx > 0 and c[idx-1] == n:
+                continue
+
+            result = recursive(c[:idx] + c[idx+1:], t - n, chosen + [n])
+            if result is not None:
+                results.append(result)
+
+    recursive(candidates, target, [])
+
+    return results
+
+combination_sum([10,1,2,7,6,1,5], 8)
+combination_sum([2,5,2,1,2], 5)
