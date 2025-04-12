f39e681032fd63af790e3e7e9840954f9ae81cf6
nahyeon-an <skgus923@gmail.com>
2025-04-12 20:03:58 +0900

    (10-string) ⭐ improve longest valid parentheses with index stack

--- 10.string/solve_java/Leetcode32.java
+++ 10.string/solve_java/Leetcode32.java
@@ -12,6 +12,31 @@ public class Leetcode32 {
         }
     }
 
+    public int improvedSolution(String s) {
+        /**
+         * 내가 생각하지 못했던 부분 : stack 에 index 를 넣자
+         * - ) 가 등장했을 때 max length 계산이 어려웠던 이유가 연결된 케이스 (()) 때문이었음. 그래서 pair 로 idx 까지 정의해버림.
+         * - stack 에 저장되는 index 는 substring ( 의 시작점이 아닌 마지막으로 unmatched 인 인덱스를 저장. (내가 한 방식에서는 +1 을 해줘야함)
+         */
+        int maxLength = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop(); // (1) last ( index (2) last unmatched ) index
+                if (stack.isEmpty()) {
+                    stack.push(i);  // new starting index
+                }
+                maxLength = Math.max(maxLength, i - stack.peek());
+            }
+        }
+
+        return maxLength;
+    }
+
     public int longestValidParentheses(String s) {
         boolean[] popArray = new boolean[s.length()];
         Stack<Pair> stack = new Stack<>();
